76e904b16160e0578127d595b31eb291f2cecea9
diegots12 <diegosvts2021@gmail.com>
2022-12-06 22:06:28 -0500

    a

---
+++ PROMEDIOSW/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PROMEDIOSW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double I, A, B, C;
+            Console.WriteLine("Favor de ingresar cuantas calificaciones quiere ingresar");
+            A= int.Parse(Console.ReadLine());
+            I = 1;
+            C = 0;
+            while (I<=A) {
+                Console.WriteLine("Ingresar promedio "+I);
+                B = Double.Parse(Console.ReadLine());
+                C = C + B;
+                I = I + 1;
+            }
+            Console.WriteLine("el promedio es " + C/A);
+        }
+    }
+}
